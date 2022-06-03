888aa10720f073d1c6287123b27066ae52f93e7b
Angelique Abacajan <ma_utsey@yahoo.com>
2022-06-02 20:11:20 -0400

    Divided project sections to 10 per screen

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+const screens = document.querySelectorAll(".screen");
+const project_links = document.querySelectorAll(".project-link");
 const start_btn = document.getElementById("start-btn");
 
 start_btn.addEventListener("click", () => screens[0].classList.add("up"));
+
+project_links.forEach(btn => {
+
+    btn.addEventListener("click", () => {
+        const img = btn.querySelector("img");
+        const src = img.getAttribute("src");
+        const alt = img.getAttribute("alt");
+
+        screens[1].classList.add("up");
+    })
+})
